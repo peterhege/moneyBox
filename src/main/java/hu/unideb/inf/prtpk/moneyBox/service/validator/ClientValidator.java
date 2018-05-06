a7c4b19b9981280b0bbd4a6ec6aee42d6fd7df14
peterhege <mr.peter.hege@gmail.com>
2018-05-06 19:01:30 +0200

    Add logger to ClientValidator

--- src/main/java/hu/unideb/inf/prtpk/moneyBox/service/validator/ClientValidator.java
+++ src/main/java/hu/unideb/inf/prtpk/moneyBox/service/validator/ClientValidator.java
@@ -1,7 +1,10 @@
 package hu.unideb.inf.prtpk.moneyBox.service.validator;
 
 import hu.unideb.inf.prtpk.moneyBox.model.Client;
+import hu.unideb.inf.prtpk.moneyBox.utility.EntityManagerFactoryUtil;
 import org.apache.commons.validator.routines.EmailValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,14 @@ import java.util.List;
  */
 public class ClientValidator implements Validator<Client> {
 
+    /**
+     * <pre>LoggerFactory a logoláshoz.</pre>
+     */
+    private static Logger logger = LoggerFactory.getLogger(EntityManagerFactoryUtil.class);
+
+    /**
+     * Hibák listája.
+     */
     private List<ErrorEnum> errorList;
 
     /**
@@ -22,7 +33,11 @@ public class ClientValidator implements Validator<Client> {
      * @param client {@link Client}
      */
     private void nameIsValid(Client client) {
-        if (client.getClientName().length() <= 3) errorList.add(ErrorEnum.SHORT_NAME);
+        logger.info("Validate Client username");
+        if (client.getClientName().length() <= 3) {
+            errorList.add(ErrorEnum.SHORT_NAME);
+            logger.warn("Client username is too short: " + client.getClientName().length());
+        }
     }
 
     /**
@@ -34,7 +49,11 @@ public class ClientValidator implements Validator<Client> {
      * @param client {@link Client}
      */
     private void passwordIsValid(Client client) {
-        if (client.getPassword().length() <= 3) errorList.add(ErrorEnum.SHORT_PASSWORD);
+        logger.info("Validate Client password");
+        if (client.getPassword().length() <= 3) {
+            errorList.add(ErrorEnum.SHORT_PASSWORD);
+            logger.warn("Client password is too short: " + client.getPassword().length());
+        }
     }
 
     /**
@@ -44,11 +63,16 @@ public class ClientValidator implements Validator<Client> {
      * @see EmailValidator
      */
     private void emailIsValid(Client client) {
-        if (!EmailValidator.getInstance().isValid(client.getEmail())) errorList.add(ErrorEnum.INVALID_EMAIL);
+        logger.info("Validate Client email address");
+        if (!EmailValidator.getInstance().isValid(client.getEmail())){
+            errorList.add(ErrorEnum.INVALID_EMAIL);
+            logger.warn("Client email is invalid! " + client.getEmail());
+        }
     }
 
     @Override
     public List<ErrorEnum> validate(Client client) {
+        logger.info("Validate Client");
         errorList = new ArrayList<>();
 
         nameIsValid(client);
