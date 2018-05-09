43609271d7108d5f4aadc150d9886892660e09bc
peterhege <mr.peter.hege@gmail.com>
2018-05-09 20:52:19 +0200

    Javadoc

--- src/main/java/hu/unideb/inf/prtpk/moneyBox/controller/converter/Converter.java
+++ src/main/java/hu/unideb/inf/prtpk/moneyBox/controller/converter/Converter.java
@@ -1,5 +1,11 @@
 package hu.unideb.inf.prtpk.moneyBox.controller.converter;
 
+/**
+ * <pre>Modellek konvertálása a rétegek között (MVVM).</pre>
+ *
+ * @param <T> View Modell
+ * @param <P> DAO Modell
+ */
 public interface Converter<T, P> {
     T from(P p);
 
