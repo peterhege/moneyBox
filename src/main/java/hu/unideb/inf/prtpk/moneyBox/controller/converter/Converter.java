1d1037336e176e37ee0a95ea079cefb4b8bd68e2
peterhege <mr.peter.hege@gmail.com>
2018-05-10 16:17:49 +0200

    Add JavaDoc comments to Converter interface

--- src/main/java/hu/unideb/inf/prtpk/moneyBox/controller/converter/Converter.java
+++ src/main/java/hu/unideb/inf/prtpk/moneyBox/controller/converter/Converter.java
@@ -3,11 +3,23 @@ package hu.unideb.inf.prtpk.moneyBox.controller.converter;
 /**
  * <pre>Modellek konvertálása a rétegek között (MVVM).</pre>
  *
- * @param <T> View Modell
- * @param <P> DAO Modell
+ * @param <V> View Modell
+ * @param <D> DAO Modell
  */
-public interface Converter<T, P> {
-    T from(P p);
+public interface Converter<V, D> {
+    /**
+     * <pre>Service-hez szükséges modell előállítása.</pre>
+     *
+     * @param d DAO-hoz szükséges modell
+     * @return View Modell
+     */
+    V from(D d);
 
-    P to(T t);
+    /**
+     * <pre>DAO-hoz szükséges modell előállítása.</pre>
+     *
+     * @param v Service-hez szükséges modell
+     * @return DAO Modell
+     */
+    D to(V v);
 }
