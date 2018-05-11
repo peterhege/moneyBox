4054e0f850bb48056ad22af7522716c5e50e6b74
peterhege <mr.peter.hege@gmail.com>
2018-05-11 17:18:45 +0200

    Create ErrorField enum

---
+++ src/main/java/hu/unideb/inf/prtpk/moneyBox/Error/ErrorField.java
@@ -0,0 +1,23 @@
+package hu.unideb.inf.prtpk.moneyBox.Error;
+
+/**
+ * A hibák helye.
+ */
+public enum ErrorField {
+    /**
+     * Felhasználév.
+     */
+    USER_NAME,
+    /**
+     * Jelszó.
+     */
+    PASSWORD,
+    /**
+     * e-mail cím;
+     */
+    EMAIL,
+    /**
+     * Felhasználóval kapcsolatos problémák
+     */
+    CLIENT
+}
